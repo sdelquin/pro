f77f4f83133724f427063cd81f1eb1cd5ec0bf66
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-09-29 13:34:52 +0100

    Make backup on flash script

--- UT0/files/flash.sh
+++ UT0/files/flash.sh
@@ -2,12 +2,22 @@
 
 sudo apt install -y vim curl git tree xclip fonts-powerline fonts-firacode psmisc autocutsel materia-gtk-theme papirus-icon-theme
 
+if [ -e ~/.vim/autoload/plug.vim ]
+ then cp ~/.vim/autoload/plug.vim ~/.vim/autoload/plug.vim.bk
+fi
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+if [ -e ~/.vimrc ]
+ then cp ~/.vimrc ~/.vimrc.bk
+fi
 curl -fLo ~/.vimrc https://raw.githubusercontent.com/sdelquin/pro/main/UT0/files/.vimrc
 vi -E -s +PlugInstall +qall
 
 sudo -- sh -c "ln -sf $HOME/.vimrc /root/.vimrc; ln -sf $HOME/.vim /root/.vim"
 
+if [ -e ~/.bashrc ]
+ then cp ~/.bashrc ~/.bashrc.bk
+fi
 curl -fLo ~/.bashrc https://raw.githubusercontent.com/sdelquin/pro/main/UT0/files/.bashrc
 source ~/.bashrc
 
@@ -17,4 +27,7 @@ systemctl --user daemon-reload
 systemctl --user enable autocutsel
 systemctl --user start autocutsel
 
+if [ -e ~/.config/xfce4/terminal/terminalrc ]
+ then cp ~/.config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc.bk
+fi
 curl -fLo ~/.config/xfce4/terminal/terminalrc https://raw.githubusercontent.com/sdelquin/pro/main/UT0/files/terminalrc
