90b4f72445278761098a5b3be48fd400973ec13c
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-09-18 20:52:57 +0100

    Migrate some contents to edubase

--- ut0/files/maintenance.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-blacklist=("openai.com" "www.openai.com" "api.openai.com" "blog.openai.com" "docs.openai.com" "forum.openai.com" "jobs.openai.com" "research.openai.com" "status.openai.com" "support.openai.com" "team.openai.com" "beta.openai.com" "chat.openai.com")
-
-for domain in "${blacklist[@]}"; do
-  iptables -A OUTPUT -p tcp -d $domain -j DROP
-done
